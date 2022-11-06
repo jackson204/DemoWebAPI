86733627f28578e2ef416331a4056b2808894589
jackson <s97403002a@gmail.com>
2022-11-06 20:28:27 +0800

    use MVC的屬性，建立基本的驗證功能 (run 起來後 可查看Schemas)

--- DemoWebAPI/Model/Order.cs
+++ DemoWebAPI/Model/Order.cs
@@ -1,16 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DemoWebAPI.Model;
 
 public sealed class Order
 {
     [FromRoute(Name = "oid")]
+    [Required]
     public int Id { get; set; }
 
     [FromQuery(Name = "name")]
     public string Name { get; set; }
+    
     [FromQuery(Name = "price")]
+    [Range(0,99)]
     public int Price { get; set; }
+    
     [FromQuery(Name = "pid")]
     public int ProductId { get; set; }
 }
