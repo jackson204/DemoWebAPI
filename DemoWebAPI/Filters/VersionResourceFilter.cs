8fc8ebd33e4fc34a7efd13e96e3cbca7ecf1ab25
jackson <s97403002a@gmail.com>
2022-11-08 21:31:34 +0800

    ResourceFilter 驗證不同的http Request path ex: 不包含有v2的 都回傳BadRequestObjectResult

---
+++ DemoWebAPI/Filters/VersionResourceFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DemoWebAPI.Filters;
+
+public sealed class VersionResourceFilter : Attribute, IResourceFilter
+{
+    //https://learn.microsoft.com/zh-tw/aspnet/core/mvc/controllers/filters?view=aspnetcore-6.0
+    // 模型系結之前執行程式碼
+    public void OnResourceExecuting(ResourceExecutingContext context)
+    {
+        if (context.HttpContext.Request.Path.Value != null && !context.HttpContext.Request.Path.Value.ToLower().Contains("v2"))
+        {
+            context.Result = new BadRequestObjectResult(new
+            {
+                Version = new[] {"This version of the API has expired. "}
+            });
+        }
+    }
+
+    public void OnResourceExecuted(ResourceExecutedContext context)
+    {
+    }
+}
